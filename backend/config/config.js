d0414590c59877e52a903dbf052a59bf4e9cd666
luciofondon <luciofondonterron@gmail.com>
2018-04-11 11:13:33 +0200

    Update config.js

--- backend/config/config.js
+++ backend/config/config.js
@@ -3,9 +3,9 @@ var config = {
   SERVER_PORT:        process.env.SERVER_PORT || '4008',
   SERVER_EXPORT:      'http://localhost:4010',
   MONGO_PATH:         process.env.MONGO_PATH || 'mongodb://localhost:27017/tfm-uex',
-  SERVER_MAIL:        'smtp.gammasg.com',
-  USER_MAIL:          'no.reply@gammasg.com',
-  PASSWORD_MAIL:      'Gamma2015',
+  SERVER_MAIL:        '',
+  USER_MAIL:          '',
+  PASSWORD_MAIL:      '',
   TOKEN_SECRET:       process.env.TOKEN_SECRET || "tfg.uex.luciofondon"
 }
 
