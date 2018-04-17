259538c6f580b6f1b74c608dc1776bfa4a7c460d
Lucio Fondón <luciofondonterron@gmail.com>
2018-04-17 16:30:39 +0200

    Update QuestionModel.js

--- backend/src/models/QuestionModel.js
+++ backend/src/models/QuestionModel.js
@@ -2,31 +2,35 @@ var mongoose = require('mongoose'),
     Schema   = mongoose.Schema;
 
 var questionSchema = new Schema({
-  description: {
-    type: String,
-    required: true
-  },
-  answers:[{
-    description: {
-      type: String
-    },
-    requirement: {
-      type: String
-    }
-  }],
-  topic: {
-  	type: Schema.ObjectId,
-  	ref: 'Topic',
-  	required: true
-  },
-  created: {
-    type: Date,
-    default: Date.now
-  } 
+	description: {
+    	type: String,
+    	required: true
+  	},
+	answers:[{
+		description: {
+		  	type: String
+		},
+		requirement: {
+			type: String
+		}
+	}],
+  	topic: {
+		type: Schema.ObjectId,
+		ref: 'Topic',
+		required: true
+  	},
+	created: {
+		type: Date,
+		default: Date.now
+	},
+	isTemplate: {
+		type: Boolean,
+		default: false
+	}
 });
 
 questionSchema.statics.load = function(questionId, callback) {
-  this.findOne({_id: questionId}).exec(callback);
+	this.findOne({_id: questionId}).exec(callback);
 };
 
 module.exports = mongoose.model('Question', questionSchema);
