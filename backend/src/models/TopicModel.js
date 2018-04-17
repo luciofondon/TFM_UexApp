bf7f0d670ffb4922678d9e0411ce67095cba304b
Lucio Fondón <luciofondonterron@gmail.com>
2018-04-17 16:31:32 +0200

    Update TopicModel.js

--- backend/src/models/TopicModel.js
+++ backend/src/models/TopicModel.js
@@ -2,23 +2,27 @@ var mongoose = require('mongoose'),
     Schema   = mongoose.Schema;
 
 var topicSchema = new Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  project: {
-  	type: Schema.ObjectId,
-  	ref: 'Project',
-  	required: true
-  },
-  created: {
-    type: Date,
-    default: Date.now
-  } 
+	name: {
+		type: String,
+		required: true
+	},
+	project: {
+		type: Schema.ObjectId,
+		ref: 'Project',
+		required: true
+	},
+	created: {
+		type: Date,
+		default: Date.now
+	},
+	isTemplate: {
+		type: Boolean,
+		default: false
+	}
 });
 
 topicSchema.statics.load = function(topicId, callback) {
-  this.findOne({_id: topicId}).exec(callback);
+	this.findOne({_id: topicId}).exec(callback);
 };
 
 
