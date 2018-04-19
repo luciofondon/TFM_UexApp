a9102b240a07956cb05c8a5da3f12f42bb68bc7c
Lucio Fondón <luciofondonterron@gmail.com>
2018-04-19 09:41:17 +0200

    Update Gruntfile.js

--- frontend/Gruntfile.js
+++ frontend/Gruntfile.js
@@ -119,13 +119,13 @@ module.exports = function(grunt){
 			dev: {
 				files: [
 		      		{expand: true, src: 'favicon.ico', dest: 'public/dist/', filter: 'isFile'},
-		      		{expand: true, src: 'indexDEV.html', dest: 'public/dist/', filter: 'isFile'}
+		      		{expand: true, src: 'indexDEV.html', dest: 'public/dist/', filter: 'isFile', rename: function(dest, src){return 'indext.html';}},
 				]
 			},
 			pro: {
 				files: [
 		      		{expand: true, src: 'favicon.ico', dest: 'public/dist/', filter: 'isFile'},
-		      		{expand: true, src: 'indexPRO.html', dest: 'public/dist/', filter: 'isFile'}
+		      		{expand: true, src: 'indexPRO.html', dest: 'public/dist/', filter: 'isFile', rename: function(dest, src){return 'indext.html';}},
 				]
 			},
 		},
