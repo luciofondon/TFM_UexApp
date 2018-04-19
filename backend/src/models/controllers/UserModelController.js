48fdfc01749e23c96104ca5abe186788f24caaa1
Lucio Fondón <luciofondonterron@gmail.com>
2018-04-19 11:01:05 +0200

    Update UserModelController.js

--- backend/src/models/controllers/UserModelController.js
+++ backend/src/models/controllers/UserModelController.js
@@ -1,1 +1,10 @@
 var UserSchema = require('mongoose').model('UserModel').schema;
+
+ animalSchema.query.byName = function(name) {
+    return this.find({ name: new RegExp(name, 'i') });
+  };
+
+  var Animal = mongoose.model('Animal', animalSchema);
+  Animal.find().byName('fido').exec(function(err, animals) {
+    console.log(animals);
+  });
