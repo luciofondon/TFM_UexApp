8014325fcc8c937f67a63f8f047994e16d6462c3
Lucio Fondón <luciofondonterron@gmail.com>
2018-04-25 18:19:36 +0200

    Update UserTest.js

--- frontend/app/test/services/UserTest.js
+++ frontend/app/test/services/UserTest.js
@@ -1,1 +1,25 @@
 
+describe('Modulo tfm.uex', function () {
+ 
+    beforeEach(function () {
+        module('tfm.uex');
+    });
+ 
+    describe('Users service', function () {
+ 
+        var userService;
+ 
+        beforeEach(function () {
+            inject(['UserService', function (service) {
+                    userService = service;
+                }
+            ]);
+        });
+ 
+        it('debe devolver una lista de dos usuarios', function () {
+            var users = userService.getAll();
+            expect(users).toBeDefined();
+            expect(users.length).toBe(2);
+        });
+    });
+});
