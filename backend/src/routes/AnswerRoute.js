d3d3d6e80dbe4f73f2fa4bbf6b3e572473605af0
Lucio Fondón <luciofondonterron@gmail.com>
2018-04-26 10:25:55 +0200

    Update AnswerRoute.js

--- backend/src/routes/AnswerRoute.js
+++ backend/src/routes/AnswerRoute.js
@@ -1,1 +1,18 @@
 
+module.exports = function(app){
+	
+	var answerController = require('../controllers/AnswerController')(),
+		systemMiddleware = require('../middlewares/SystemMiddleware');
+
+	app.route('/answer/:questionId')
+		.get(systemMiddleware.rolAdmin, answerController.read)
+		.put(systemMiddleware.rolAdmin, answerController.update)
+        .delete(systemMiddleware.rolAdmin, answerController.delete);
+        
+    app.route('/answers/question/:questionId')
+        .get(systemMiddleware.rolAdmin, answerController.readAllByTopic)
+		.post(systemMiddleware.rolAdmin, answerController.createByTopic);  
+    
+   // app.param('questionId', answerController.loadQuestion);
+        
+}
