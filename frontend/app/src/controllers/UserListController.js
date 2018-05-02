2f42d65bbbc9ea527e9f30555037c2cf35f57894
Lucio Fondón <luciofondonterron@gmail.com>
2018-05-02 15:06:12 +0200

    Update UserListController.js

--- frontend/app/src/controllers/UserListController.js
+++ frontend/app/src/controllers/UserListController.js
@@ -1,7 +1,7 @@
 
 angular.module('tfm.uex').controller('UserListController',
-    ['UserService', 'RolService', 'ProjectService', 'BootstrapTableService', '$ngConfirm', '$scope',
-        function(UserService, RolService, ProjectService, BootstrapTableService, $ngConfirm, $scope){
+    ['UserService', 'RolService', 'ProjectService', 'BootstrapTableService', '$ngConfirm', '$scope', 'Upload',
+        function(UserService, RolService, ProjectService, BootstrapTableService, $ngConfirm, $scope, Upload){
 	var vm = this;
 	vm.mode = 1;
 	vm.user = {};
@@ -169,5 +169,19 @@ angular.module('tfm.uex').controller('UserListController',
             });
         }
     }
+	
+	function upload(file){
+        Upload.upload({
+            url: 'upload/url',
+            data: {file: file, 'username': "Lucio"}
+        }).then(function (resp) {
+            console.log('Success ' + resp.config.data.file.name + 'uploaded. Response: ' + resp.data);
+        }, function (resp) {
+            console.log('Error status: ' + resp.status);
+        }, function (evt) {
+            var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+            console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
+        });
+    };
 
 }]);
