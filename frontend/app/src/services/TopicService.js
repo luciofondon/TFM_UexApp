f8e07091a1de25c5ce624cca348469792e9bc526
Lucio Fondón <luciofondonterron@gmail.com>
2018-05-02 15:14:55 +0200

    Update TopicService.js

--- frontend/app/src/services/TopicService.js
+++ frontend/app/src/services/TopicService.js
@@ -1,13 +1,34 @@
 angular.module('tfm.uex').factory('TopicService', ['$http', function($http){
     return {
+		readTopic: function(topicId){
+			return readTopic(topicId);
+		},
 		createTopic: function(projectId, topic){
 			return createTopic(projectId, topic);
 		},
 		getTopics: function(projectId){
 			return getTopics(projectId);
+		},
+		deleteTopic: function(topicId){
+			return deleteTopic(topicId);
+		},
+		updateTopic: function(topic){
+			return updateTopic(topic);
 		}
 	};
-
+	
+	function readTopic(topicId){
+		return $http.get('/api/topic/' + topicId);
+	}
+	
+	function updateTopic(topic){
+		return $http.put('/api/topic/' + topic._id, topic);
+	}
+	
+	function deleteTopic(topicId){
+		return $http.delete('/api/topic/' + topicId);
+	}
+	
 	function createTopic(projectId, topic){
 		return $http.post('/api/topics/project/' + projectId, topic);
 	}
