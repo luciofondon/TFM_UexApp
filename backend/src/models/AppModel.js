64cd826e126b8972aa9a21899395e93eca1a0432
Lucio Fondón <luciofondonterron@gmail.com>
2018-05-03 13:46:56 +0200

    Create AppModel.js

---
+++ backend/src/models/AppModel.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose'),
+    Schema   = mongoose.Schema;
+
+var appSchema = new Schema({
+	name: {
+		type: String,
+		required: true
+	},
+	description: {
+		type: String
+	},
+	project: {
+		type: Schema.ObjectId,
+		ref: "Project"
+	},
+	created: {
+		type: Date,
+		default: Date.now,
+		required:true
+	}
+});
+
+appSchema.statics.load = function(appId, callback){
+	this.findOne({_id: appId}).exec(callback);
+};
+
+module.exports = mongoose.model('App',appSchema);
