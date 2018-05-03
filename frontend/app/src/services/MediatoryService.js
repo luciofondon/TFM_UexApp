b22ee5b7a5d9bf36c4c478b9c88e2f920e53b527
Lucio Fondón <luciofondonterron@gmail.com>
2018-05-03 09:30:31 +0200

    Create MediatoryService.js

---
+++ frontend/app/src/services/MediatoryService.js
@@ -0,0 +1,34 @@
+angular.module('tfm.uex').service('MediatoryService', ['$http', function($http){
+
+	return {
+		getApps: function(){
+			return getApps();
+		},
+    checkComunication: function(config){
+			return checkComunication(config);
+		},
+    getProjects: function(config){
+			return getProjects(config);
+		},
+    createProject: function(config, project){
+    	return getProjects(config, project);
+    }
+	};
+
+	function getApps(){
+		return $http.get('/api/mediatory/apps');
+	}
+  
+  function checkComunication(config){
+		return $http.get('/api/mediatory/apps', config);
+	}
+  
+  function getProjects(config){
+		return $http.get('/api/mediatory/projects', config);
+	}
+  
+  function createProject(config){
+		return $http.post('/api/mediatory/projects', {config: config, project: project});
+	}
+
+}]);
