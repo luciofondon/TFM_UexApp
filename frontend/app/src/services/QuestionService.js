7dea78e823f5444bde26c4ab857b205c19ded237
Lucio Fondón <luciofondonterron@gmail.com>
2018-05-07 15:21:59 +0200

    Update QuestionService.js

--- frontend/app/src/services/QuestionService.js
+++ frontend/app/src/services/QuestionService.js
@@ -11,9 +11,37 @@ angular.module('tfm.uex').factory('QuestionService', ['$http', function($http){
 		},
 		readQuestion: function(questionId){
 			return readQuestion(questionId);
+		},
+		createQuestionAsociate: function(topicId, question){
+			return createQuestionAsociate(topicId, question);
+		},
+		updateQuestionAsociate: function(question){
+			return updateQuestionAsociate(question);
+		},
+		deleteQuestionAsociate: function(questionId){
+			return deleteQuestionAsociate(questionId);
+		},
+		readQuestionAsociate: function(questionId){
+			return readQuestionAsociate(questionId);
 		}
 	};
 
+	function createQuestionAsociate(topicId, question){
+		return $http.post('/api/questions/topic/' + topicId, question);
+	}
+
+	function updateQuestionAsociate(question){
+		return $http.put('/api/question/' + question._id, question);
+	}
+
+	function deleteQuestionAsociate(questionId){
+		return $http.delete('/api/question/' + questionId);
+	}
+
+	function readQuestionAsociate(questionId){
+		return $http.get('/api/question/' + questionId);
+	}
+	
 	function createQuestion(topicId, question){
 		return $http.post('/api/questions/topic/' + topicId, question);
 	}
