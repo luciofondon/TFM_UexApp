5f164bbd106a3426e2a29db1f819f09d0c1d9a97
Lucio Fondón <luciofondonterron@gmail.com>
2018-05-09 09:45:50 +0200

    Update ProjectService.js

--- frontend/app/src/services/ProjectService.js
+++ frontend/app/src/services/ProjectService.js
@@ -20,11 +20,39 @@ angular.module('tfm.uex').factory('ProjectService', ['$http', function($http){
 		},
 		createAppProject: function(projectId, app){
 			return createAppProject(projectId, app);
-		}
+		},
+		getAppsProject: function(){
+			return getAppsProject();
+		},
+		getAppProject: function(projectId, appId){
+			return getAppProject(projectId, appId);
+		},
+		deleteAppProject: function(projectId, appId){
+			return getAppProject(projectId, appId);
+		},
+		updateAppProject: function(projectId, app){
+			return createAppProject(projectId, app);
+		},
 	};
+	
+	function getAppsProject(){
+		return $http.get('/api/projects/apps');
+	}
+	
+	function getAppProject(projectId, appId){
+		return $http.get('/api/projects/apps' +projectId + '/' + appId);
+	}
+	
+	function deleteAppProject(projectId, appId){
+		return $http.get('/api/projects/apps' +projectId + '/' + appId);
+	}
+	
+	function updateAppProject(projectId, app){
+		return $http.put('/api/project/app/' + projectId + '/' + app._id, app);
+	}
 
 	function createAppProject(projectId, app){
-		return $http.post('/api/project/app/' + projectId, app);
+		return $http.post('/api/project/apps/' + projectId, app);
 	}
 
 	function generateProject(templateId, project){
@@ -50,8 +78,5 @@ angular.module('tfm.uex').factory('ProjectService', ['$http', function($http){
 	function updateProject(project){
 		return $http.put('/api/project/' + project._id, project);
 	}
-
-
-
 
 }]);
