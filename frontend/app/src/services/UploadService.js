bc82e83daa521646a7f19b306e41c4d77a0f19e1
Lucio Fondón <luciofondonterron@gmail.com>
2018-05-15 09:23:33 +0200

    Create UploadService.js

---
+++ frontend/app/src/services/UploadService.js
@@ -0,0 +1,27 @@
+angular.module('tfm.uex').service('UplodadService', ['Upload', function(Upload) {
+
+	return {
+		uploadFile: function(file){
+			return uploadFile(file);
+		}
+	};
+  
+  function upload(callback){
+		if(vm.user.image != undefined){
+			Upload.upload({
+				url: '/api/user/upload',
+				file: vm.user.image
+			}).then(function (response) {
+				callback(response.status, response.data.name);
+			}, function (response) { // Error
+				callback(response.status);
+			}, function (evt) {
+				var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+			});
+		}else{
+			callback(200, "");
+		}
+  }
+  
+}]);
+  
